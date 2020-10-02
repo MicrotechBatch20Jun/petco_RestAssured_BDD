4c616d245e49a365fdd0fc9932a85b4cae3e8cf9
Sohani <Sohani@DESKTOP-Q4ILRRD>
2020-10-02 15:35:23 -0400

    New Project

---
+++ petco_RestAssured_BDD/src/test/java/petco_RestAssured_BDD/POST_Request.java
@@ -0,0 +1,55 @@
+package petco_RestAssured_BDD;
+
+import java.util.HashMap;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class POST_Request {
+	
+	public static HashMap data = new HashMap();
+	
+	@BeforeClass
+	public void postdata() {
+		data.put("id", 600);
+		data.put("username", "jdoe");
+		data.put("firstName", "John");
+		data.put("lastName", "Doe");
+		data.put("email", "jdoe@gmail.com");
+		data.put("password", "jdoe");
+		data.put("phone", "9175004455");
+		data.put("userStatus", 0);
+		
+		RestAssured.baseURI= "https://petstore.swagger.io/v2";
+		RestAssured.basePath = "/user";
+	}
+	@Test
+	public void testPost() {
+		
+		given()
+			.header("Content-Type", "application/json")
+			.body(data)
+			
+		.when()
+			.post()
+			
+		.then()
+			.assertThat()
+				.statusCode(200)
+				.and()
+				.header("Content-Type", "application/json")
+				.and()
+				.header("Transfer-Encoding", "chunked")
+				.and()
+				.body("code", equalTo(200))
+				.and()
+				.body("type", equalTo("unknown"))
+				.and()
+				.body("message", equalTo("600"));
+		
+		
+	}
+}
